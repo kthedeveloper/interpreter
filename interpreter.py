0edfc554f03b1bec32ad0a2b1c9f236da6fc1a9d
kthedeveloper <ks.ryabov@gmail.com>
2023-12-16 12:05:23 +0200

    Supports multiplication and division operations

--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,5 @@
 
-INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
+INTEGER, MUL, DIV, EOF = 'INTEGER', 'MUL', 'DIV', 'EOF'
 
 
 class Token(object):
@@ -50,7 +50,7 @@ class Interpreter(object):
         return int(result)
 
     def get_next_token(self):
-        """Lexical analyzer (also known as scanner or tokenizer)
+        """Lexical analyzer
 
         This method is responsible for breaking a sentence
         apart into tokens.
@@ -64,13 +64,13 @@ class Interpreter(object):
             if self.current_char.isdigit():
                 return Token(INTEGER, self.integer())
 
-            if self.current_char == '+':
+            if self.current_char == '*':
                 self.advance()
-                return Token(PLUS, '+')
+                return Token(MUL, '*')
 
-            if self.current_char == '-':
+            if self.current_char == '/':
                 self.advance()
-                return Token(MINUS, '-')
+                return Token(DIV, '/')
 
             self.error()
 
@@ -93,14 +93,14 @@ class Interpreter(object):
         self.current_token = self.get_next_token()
 
         result = self.term()
-        while self.current_token.type in (PLUS, MINUS):
+        while self.current_token.type in (MUL, DIV):
             token = self.current_token
-            if token.type == PLUS:
-                self.eat(PLUS)
-                result = result + self.term()
-            elif token.type == MINUS:
-                self.eat(MINUS)
-                result = result - self.term()
+            if token.type == MUL:
+                self.eat(MUL)
+                result = result * self.term()
+            elif token.type == DIV:
+                self.eat(DIV)
+                result = result / self.term()
 
         return result
 
